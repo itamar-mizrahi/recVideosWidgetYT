bdc4903137254bf9cfdec528c219c492468ec98c
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2023-04-18 15:32:32 +0300

    TODO:android sdk

--- recVideosWidgetYT/components/RecommendedVideosWidget.js
+++ recVideosWidgetYT/components/RecommendedVideosWidget.js
@@ -1,73 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import YouTubePlayer from 'react-native-youtube';
+import React, { useEffect, useState, useCallback } from 'react';
+import YouTubeIframe from 'react-native-youtube-iframe';
 
 const RecommendedVideosWidget = () => {
-  const [videos, setVideos] = useState([]);
+  const [recommendedVideos, setRecommendedVideos] = useState([]);
 
+  const onStateChange = useCallback((state) => {
+    if (state === "ended") {
+      setPlaying(false);
+      Alert.alert("video has finished playing!");
+    }
+  }, []);
   useEffect(() => {
-    // Fetch recommended videos from your API or YouTube Data API
-    fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyDrfN2gc4k5JQTQqQt9PXVV180cMh1vlTE')
-      .then(response => response.json())
-      .then(data => setVideos(data.items))
-      .catch(error => console.error(error));
+    const fetchVideos = async () => {
+      const API_KEY = 'AIzaSyD1mJMs5lo4Dytviza92X0hYkcW0qqLyJI';
+      const CHANNEL_ID = 'UCdUAx5UaL1t6dc_aq5C7wWg';
+      const MAX_RESULTS = 10;
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=${MAX_RESULTS}&order=relevance&type=video&key=${API_KEY}`
+      );
+      
+      const data = await response.json();
+      console.log(data);
+      const videos = data.items.map((item) => item.id.videoId);
+      setRecommendedVideos(videos);
+    };
+
+    fetchVideos();
   }, []);
 
-  const renderItem = ({ item }) => (
-    <View style={styles.videoContainer}>
-      <YouTubePlayer
-        apiKey="AIzaSyDrfN2gc4k5JQTQqQt9PXVV180cMh1vlTE"
-        videoId={item.id.videoId}
-        style={styles.videoPlayer}
-      />
-      <Text style={styles.videoTitle}>{item.snippet.title}</Text>
-    </View>
-  );
-
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Recommended Videos</Text>
-        <Ionicons name="ios-arrow-forward" size={24} color="black" />
-      </View>
-      <FlatList
-        data={videos}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.videoId}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
-    </View>
+    <YouTubeIframe
+      videoIds={recommendedVideos}
+      webViewStyle={{ height: 150 }}
+      onChangeState={onStateChange}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    margin: 10,
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-  headerText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  videoContainer: {
-    marginRight: 10,
-  },
-  videoPlayer: {
-    width: 150,
-    height: 100,
-  },
-  videoTitle: {
-    marginTop: 5,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
 
 export default RecommendedVideosWidget;
